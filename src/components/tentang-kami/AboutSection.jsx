dcaec48c3afbe3c0b5bd88ca230fac77e44594de
rprasetya <102443842+rprasetya@users.noreply.github.com>
2025-10-20 21:48:04 +0800

    edit profile

--- src/components/tentang-kami/AboutSection.jsx
+++ src/components/tentang-kami/AboutSection.jsx
@@ -96,7 +96,7 @@ export default function AboutSection() {
           </div>
           <div className="flex-1 text-center md:text-left">
             <h2 className="text-3xl font-bold text-gray-800 mb-2">Yunda Zuliarsih</h2>
-            <p className="text-lg text-[#1B602F] font-semibold mb-4"></p>
+            <p className="text-lg text-[#1B602F] font-semibold mb-4">Ketua MALIPE</p>
             <p className="text-gray-600 leading-relaxed">
             Mendukung jalannya organisasi konservasi penyu dengan ketelitian dan tanggung jawab tinggi. Berperan aktif dalam mendukung kelancaran program konservasi serta menjaga koordinasi yang efektif antar anggota untuk mewujudkan tujuan pelestarian ekosistem laut yang berkelanjutan.
             </p>
