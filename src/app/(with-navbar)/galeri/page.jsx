40e485d9d6015d8e75edbceab134589a31710643
rprasetya <102443842+rprasetya@users.noreply.github.com>
2025-10-20 14:48:40 +0800

    remove partner section

--- src/app/(with-navbar)/galeri/page.jsx
+++ src/app/(with-navbar)/galeri/page.jsx
@@ -1,13 +1,11 @@
 import GallerySection from "@/components/galeri/GallerySection";
 import Hero from "@/components/galeri/Hero";
-import PartnerSection from "@/components/galeri/PartnerSection";
 
 export default function Page() {
   return (
     <div className="">
       <Hero/>  
       <GallerySection/>
-      {/* <PartnerSection/> */}
     </div>
   );
 }
